7c65d8e19b926414073579ca2ee7630171eab084
Barnaby Gray <barnaby@pickle.me.uk>
2015-12-15 23:03:47 +0000

    Day 7

---
+++ day07/main.nim
@@ -0,0 +1,43 @@
+import re
+import strutils
+import tables
+
+type WireProc = proc(): uint16
+
+var wires = initTable[string, WireProc]()
+var cache = initTable[string, uint16]()
+
+proc lookup(name: string): uint16 =
+  if name notin cache:
+    result = if name.match(re"\d+"):
+        uint16(parseInt(name))
+      else:
+        wires[name]()
+    cache[name] = result
+  else:
+    result = cache[name]
+
+for line in lines "input.txt":
+  var m = newSeq[string](3)
+  if line.match(re"(\w+)\s->\s(\w+)", m):
+    wires[m[1]] = proc(): uint16 = lookup(m[0])
+  elif line.match(re"NOT\s(\w+)\s->\s(\w+)", m):
+    wires[m[1]] = proc(): uint16 = not lookup(m[0])
+  elif line.match(re"(\w+)\sAND\s(\w+)\s->\s(\w+)", m):
+    wires[m[2]] = proc(): uint16 = lookup(m[0]) and lookup(m[1])
+  elif line.match(re"(\w+)\sOR\s(\w+)\s->\s(\w+)", m):
+    wires[m[2]] = proc(): uint16 = lookup(m[0]) or lookup(m[1])
+  elif line.match(re"(\w+)\sLSHIFT\s(\d+)\s->\s(\w+)", m):
+    wires[m[2]] = proc(): uint16 = lookup(m[0]) shl uint16(parseInt(m[1]))
+  elif line.match(re"(\w+)\sRSHIFT\s(\d+)\s->\s(\w+)", m):
+    wires[m[2]] = proc(): uint16 = lookup(m[0]) shr uint16(parseInt(m[1]))
+  else:
+    echo "Bad: ", line
+
+var ans1 = wires["a"]()
+echo "Answer #1: ", ans1
+
+# override b with value from a, and calculate new signal for a
+cache = initTable[string, uint16]()
+cache["b"] = ans1
+echo "Answer #2: ", wires["a"]()
